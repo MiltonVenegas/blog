cd181c11f6fe71ade211431cac21c10b60252895
MiltonVenegas <a20210138@utem.edu.mx>
2024-02-12 11:53:31 -0600

    Fallo

---
+++ blog/Controllers/MensajeController.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using TuProyecto.Models;
+
+namespace blog.Controllers
+{
+    public class MensajeController : Controller
+    {
+        private static List<Mensaje> mensajes = new List<Mensaje>();
+
+        // GET: Mensaje
+        public ActionResult Index()
+        {
+            return View(mensajes);
+        }
+
+        // GET: Mensaje/Crear
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        // POST: Mensaje/Crear
+        [HttpPost]
+        public ActionResult Crear(Mensaje mensaje)
+        {
+            mensaje.FechaPublicacion = DateTime.Now;
+            mensajes.Add(mensaje);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ blog/Models/Mensaje.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TuProyecto.Models
+{
+    public class Mensaje
+    {
+        public string Nombre { get; set; }
+        public string Contenido { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+    }
+}
